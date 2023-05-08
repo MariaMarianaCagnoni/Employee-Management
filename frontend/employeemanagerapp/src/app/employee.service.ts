e4950caf101bad7eec85d9b1e802f3a862e268f8
mariana <mariamariana.cagnoni@gmail.com>
2023-05-08 10:44:46 -0300

    service created

--- frontend/employeemanagerapp/src/app/employee.service.ts
+++ frontend/employeemanagerapp/src/app/employee.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Employee } from './employee';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,21 @@ export class EmployeeService {
 
   constructor(private http: HttpClient) { }
 
-  public getEmployees(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}/employee`);
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiUrl}/employee`);
+
+  }
+
+  public addEmployees(emplpoyee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiUrl}/employee`, emplpoyee);
+
+  }
+  public updateEmployees(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiUrl}/employee/update`, employee);
+
+  }
+  public deleteEmployees(employeeId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/employee/${employeeId}`);
 
   }
 }
